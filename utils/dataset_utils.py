4c4d6e01933c944d52d3832e271375668e90b169
AidenParkerr <aidenparker1998@live.co.uk>
2024-04-17 14:00:50 +0100

    update

--- utils/dataset_utils.py
+++
@@ -1,6 +0,0 @@
-class CustomerDatasetLoader:
-  def __init__(self, data_path):
-    self.data_path = data_path
-
-  def load(self):
-    raise NotImplementedError("Method not implemented")
--- yolo/training.py
+++
@@ -1,9 +0,0 @@
-
-
-class YOLOTrainer:
-  def __init__(self, model, config):
-    self.model = model
-    self.config = config
-
-  def train(self):
-    raise NotImplementedError("Method not implemented")
